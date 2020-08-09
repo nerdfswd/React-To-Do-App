210f92e1787a7946ffdff5b040ebaab3a250c0c3
sabiyatabassum <sabiyatabassum99@gmail.com>
2020-08-09 19:11:11 +0530

    Updates

---
+++ precache-manifest.96bf1356a224c0160cadeb1b231462bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eabab7f3825c49df2f6d87884d947eb3",
+    "url": "/React-To-Do-App/index.html"
+  },
+  {
+    "revision": "7ae33e6ae4754759722c",
+    "url": "/React-To-Do-App/static/css/main.9ea597f4.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/React-To-Do-App/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-To-Do-App/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae33e6ae4754759722c",
+    "url": "/React-To-Do-App/static/js/main.ee30322d.chunk.js"
+  },
+  {
+    "revision": "5364f329f49e991249ab",
+    "url": "/React-To-Do-App/static/js/runtime-main.0569825c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-To-Do-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
